c69205b0f140a11b432f6aae1c3df60c282578b3
SuyashKerkar <suyashkasdf@gmail.com>
2025-02-13 12:20:30 +0530

    db.js updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,38 @@
+// const mysql = require("mysql2");
+
+// require("dotenv").config();
+
+// const db = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
+
+// module.exports = db;
 const mysql = require("mysql2");
 
-require("dotenv").config();
-
-const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST, // Clever Cloud MySQL Host
+  user: process.env.DB_USER, // Clever Cloud MySQL User
+  password: process.env.DB_PASSWORD, // Clever Cloud MySQL Password
+  database: process.env.DB_NAME, // Clever Cloud MySQL Database Name
   port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
 
-module.exports = db;
+connection.connect((err) => {
+  if (err) {
+    console.error("❌ MySQL Connection Failed:", err);
+    return;
+  }
+  console.log("✅ Connected to Clever");
+});
+
+module.exports = connection;
